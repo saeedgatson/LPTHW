0b574a903bc78f269e5c917ef90def5baf11e4e8
Saeed Gatson <saeedgatson@gmail.com>
2017-01-02 05:56:22 -0800

    Add Exercise 3

---
+++ ex3.py
@@ -0,0 +1,31 @@
+# This writes out what we are doing to the screen.
+print "I will now count my chickens:"
+
+# Calculates how many hens and roosters we have.
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25 * 3 % 4
+
+# Writes on the screen that we will now count eggs
+print "Now I will count the eggs:"
+
+# Calculates and shows the number of eggs
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4.0 + 6
+
+# Ask a question
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Evaluates if 3 + 2 < 5 - 7
+print 3 + 2.0 < 5 - 7.0
+
+# Breakdown question to understand what happened.
+print "What is 3 + 2?", 3.0 + 2
+print "What's 5 - 7?", 5 - 7.0
+
+# Shows reasoning on screen
+print "oh, that's why it's False."
+
+# Show more info about question
+print "How about some more."
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2
+print "Is it less or equal?", 5 <= -2.0
