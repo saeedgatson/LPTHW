1447c733a20df255867a47116f891953a85bd534
Saeed Gatson <saeedgatson@gmail.com>
2017-01-05 05:54:22 -0800

    Add Work From Exercise 11

---
+++ ex11.py
@@ -0,0 +1,10 @@
+print "How old are  you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weight?",
+weight = raw_input()
+print "How your current mood?",
+mood = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy. Current mood %r" % (age, height, weight, mood)
